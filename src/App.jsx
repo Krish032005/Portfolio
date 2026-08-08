bf264511b017fe83fcd5d2a0dcde8fd8e7c21288
Krish Mehta <krish.mehta.20053@gmail.com>
2026-08-08 23:40:49 +0530

    error Resolve

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Projects from './Components/Projects';
 import Skills from './Components/Skills';
 import Footer from './Components/Footer';
 import Demo from './Components/Demo';
-import DemoFooter from './Components/DemoFooter';
 const App = () => {
   return (
     <div className='w-full min-h-screen bg-black  text-white '>
